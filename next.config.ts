deb49133b7c187e2aff6ed6f6069624d7a21736f
Ravi Gupta <raviranjankumargupta@Ravis-MacBook-Air.local>
2026-02-28 03:29:37 +0530

    fix: allow youtube domains in next.config.ts to fix broken video thumbnails

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,25 @@ const nextConfig: NextConfig = {
       },
     ]
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
